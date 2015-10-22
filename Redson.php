f39070548f212ec9ef6a6bff8b981a3bfad92373
Den <any.zicky@gmail.com>
2015-10-22 22:14:32 +0300

    add header description and construct/destruct method class

--- Redson.php
+++ Redson.php
@@ -1,3 +1,35 @@
 <?php
 
-//Redson class interface for Redis
+/**
+ * Redson it's USSR Superman :)
+ * Redson just simple interface for Redis DB
+ * @author Suraev Denis <any.zicky@gmail.com>
+ * @copyright 2015 Suraev Denis <any.zicky@gmail.com>
+ * @package Redson
+ */
+
+namespace Redson;
+
+/**
+* 
+*/
+class Redis
+{
+
+	private $__redis;
+	private $__scheme = "redis://";
+	public $host;
+	public $port;
+	
+	function __construct($host = $this->$__scheme."localhost", $port = "6379")
+	{
+		$this->$__redis = fsockopen(, $arUrl['port'], $errno,$errstr);
+		if(!$this->$__redis) {
+		  die($errno . "(" . $errstr . ")");
+		}
+	}
+
+	function __destruct(){
+		fclose($this->$__redis);
+	}
+}
